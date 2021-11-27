f6dc2bb788d94f840684849c01153dbdbd8a0cb1
mmezic <matejmmezera@gmail.com>
2021-11-27 02:17:04 +0100

    update

--- !IDs.md
+++ !IDs.md
@@ -4,6 +4,16 @@ todo:
 projetí s ne-symm RBM
 9618677 - 4
 9618678 - 8
-9618679 - 16
-9618680 - 20
-9618681 - 36
+9618679 - 8
+9618680 - 8
+9618681 - 8
+
+-- lol, bez symetrií to seběhlo strašně rychle
+párty hárder:
+
+9620392 - 8 (nyni uz spravna mrizka)
+9620393 - 16
+9620394 - 20
+9620395 - 36
+
+9620396 - 8 ... korekce pro RBMSymm (je to ve slořce order_parameters)
--- RBM36.py
+++ RBM36.py
@@ -1,6 +1,6 @@
 import numpy as np
 """lattice"""	
-SITES    = 8            # 4, 8, 16, 20 ... number of vertices in a tile determines the tile shape 	
+SITES    = 36            # 4, 8, 16, 20 ... number of vertices in a tile determines the tile shape 	
 JEXCH1   = .8            # nn interaction	
 JEXCH2   = 1            # nnn interaction	
 """machine learning"""	
@@ -10,7 +10,7 @@ MACHINE = 'RBM'         # 'RBM', 'RBMSymm'
 DTYPE = np.complex128   # type of weights in neural network
 ALPHA = 16              # N_hidden / N_visible	
 ETA   = .005            # learning rate (0.01 usually works)	
-SAMPLES = 1500	
-NUM_ITER = 1500	
+SAMPLES = 2000	
+NUM_ITER = 2000	
 VERBOSE = False         # should we print more detailed results
 STEPS = np.arange(0,1.21,step=0.4)	
