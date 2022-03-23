5c8848f101b52304fbfa6481a2b79ced565fb59e
mmezic <matejmmezera@gmail.com>
2022-03-23 19:22:26 +0100

    upda

--- test-run.sh
+++ test-run.sh
@@ -8,10 +8,11 @@ script="main-models.py"
 
 # setting the automatical cleaning of the SCRATCH
 # trap 'clean_scratch' TERM EXIT
-# module load NetKet/3.3.2.post1-OpenMPI-4.1.1-CUDA-11.6.0
-module load Python/3.9.6-GCCcore-11.2.0-NetKet
-module load intel/2020a
+module load NetKet/3.3.2.post1-OpenMPI-4.1.1-CUDA-11.6.0
+# module load Python/3.9.6-GCCcore-11.2.0-NetKet
+# module load intel/2020a
 # create scratch
+export OMP_NUM_THREADS=36
 SCR="/lscratch/$PBS_JOBID"
 mkdir -p $SCR
 
@@ -23,11 +24,12 @@ echo "cd " $SCR >>$SOURCE/stroj.txt
 echo "ssh " `hostname` >>$SOURCE/stroj.txt
 
 # copy script to the scratch directory
-cp $PBS_O_WORKDIR/config*.py $PBS_O_WORKDIR/lattice_and_ops.py $PBS_O_WORKDIR/GCNN_Nomura.py $PBS_O_WORKDIR/$script $SCR
+cp $PBS_O_WORKDIR/config-mod*.py $PBS_O_WORKDIR/lattice_and_ops.py $PBS_O_WORKDIR/GCNN_Nomura.py $PBS_O_WORKDIR/$script $SCR
 # change directory to the sratch
 cd $SCR || exit
 # run simulation
-mpirun -np 36 python $script -f config-models16
+# mpirun -np 36 python $script -f config-models16
+python $script -f config-models16
 # copy results to my directory
 cp *.log *.mpack $PBS_O_WORKDIR
 # remove files from the scratch
