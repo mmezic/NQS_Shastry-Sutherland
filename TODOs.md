efa690cbc9bf197e0af05e6e562a09ee9be7cc7c
mmezic <matejmmezera@gmail.com>
2022-03-29 15:22:35 +0200

    upd

---
+++ TODOs.md
@@ -0,0 +1,18 @@
+# My TODO list
+
+## moje TODO
+- udělat obrázek popisující průměrování energií z posledních 100 iterací a záporné hodnoty zpsůobené asi numerickou přesnotsí
+
+
+TODO:
+- dogenerovat tabulku a vybrat model, který použiu na $H_Z$
+- zkusit dva modely z tabulky pustit s SAMPLES=5000 (do appendixu) a podívat se, jak hodně to zlepší accuracy
+- do tabulky už nepřidávat zmenšování learning ratu
+- SAMPLES mění finální rozptyl nad exaktní energií
+
+TODO:
+- dokončit tabulku
+- vybrat si jeden nejlepší model, který potom pustím na případu s mag polem
+
+## thesis text
+- Zmínit článek `Neural tensor contractions and the expressive power of deep neural quantum states` v úvodu
--- config-models16.py
+++ config-models16.py
@@ -12,12 +12,12 @@ MACHINE = 'RBM'         # 'RBM', 'RBMSymm'
 DTYPE = np.complex128   # type of weights in neural network
 ALPHA = 2              # N_hidden / N_visible	
 ETA   = .01            # learning rate (0.01 usually works)	
-SAMPLES = 2000
-NUM_ITER = 1000
+SAMPLES = 1000
+NUM_ITER = 400
 N_PRE_ITER = 50        # number of iteration before checking for convergence to speed up the process if the model is already pre-trained
 VERBOSE = True         # should we print more detailed results
 STEPS = np.arange(0,1.95,step=0.1)
-INDICES = range(11,38) #range(38)	
+INDICES = range(45,50) #range(38)	
 STEPS_A = [int(SITES/2), SITES, SITES*2, SITES*4]   # grid search across ALPHAs (used only in main-gridSearch.py)
 STEPS_E = [1,0.1,0.01,0.001,0.0001]                 # grid search across ETAs   (used only in main-gridSearch.py)
 num_layers = 2
