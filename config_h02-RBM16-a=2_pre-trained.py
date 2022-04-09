e32c1045b62c8f254d50ec686cdd1ca65f9106d4
mmezic <matejmmezera@gmail.com>
2022-04-09 14:29:47 +0200

    upd

--- config_h02-RBM16-a=2_pre-trained.py
+++ config_h02-RBM16-a=2_pre-trained.py
@@ -1,6 +1,6 @@
 import numpy as np
 """lattice"""	
-SITES    = 16            # 4, 8, 16, 20 ... number of vertices in a tile determines the tile shape 	
+SITES    = 8            # 4, 8, 16, 20 ... number of vertices in a tile determines the tile shape 	
 JEXCH1   = 0.2            # nn interaction	
 JEXCH2   = 1            # nnn interaction	
 H_Z      = 0
@@ -13,8 +13,9 @@ ALPHA = 2              # N_hidden / N_visible
 ETA   = .01            # learning rate (0.01 usually works)	
 SAMPLES = 1500	
 NUM_ITER = 700
-N_PRE_ITER = 10        # number of iteration before checking for convergence to speed up the process if the model is already pre-trained
-VERBOSE = False         # should we print more detailed results
+N_PRE_ITER = 20        # number of iteration before checking for convergence to speed up the process if the model is already pre-trained
+N_POST_ITER = 50
+VERBOSE = True         # should we print more detailed results
 STEPS = np.arange(0,2.01,step=0.1)	
 STEPS_A = [int(SITES/2), SITES, SITES*2, SITES*4]   # grid search across ALPHAs (used only in main-gridSearch.py)
 STEPS_E = [1,0.1,0.01,0.001,0.0001]                 # grid search across ETAs   (used only in main-gridSearch.py)
